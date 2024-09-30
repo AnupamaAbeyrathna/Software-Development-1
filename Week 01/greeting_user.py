cdd887573bd2f095440c70a0b29cd8dfa9489382
AnupamaSAbeyrathna <anupamas.abeyrathna@gmail.com>
2024-09-30 07:53:29 +0530

    first commit

---
+++ Week 01/greeting_user.py
@@ -0,0 +1,6 @@
+#Write a program that asks for a user’s name and age and prints a message using both inputs.
+
+name = str(input("Enter your name : ")) # name input from user
+age =   int(input("Enter your age : ")) # age input from user
+
+print(f"Hello {name}, you are {age} years old.") # print the message using both inputs
---
+++ Week 01/sum.py
@@ -0,0 +1,8 @@
+#Write a program that takes two numbers and prints their sum.
+
+num1 = int(input("Enter the first number: ")) #input() function is used to take input from the user
+num2 = int(input("Enter the second number: ")) #int() function is used to convert the input into integer
+
+sum = num1 + num2 #sum of the two numbers
+
+print("The sum of the two numbers is: ", sum) #print() function is used to display the output
